ce1a97e8ce4f4936047b325a87c079dd1ee64a34
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-04-15 23:13:19 +0300

    Додав трохи атрибутів і методів в клас компані

--- LabaNomer5/Company.cs
+++ LabaNomer5/Company.cs
@@ -9,27 +9,32 @@ namespace LabaNomer5
     {
         public string Name { get; set; }
         public List<string> Synonyms { get; set; }
-        public DateTime DateFounded { get; set; } // "
-
-        public Company(string name, List<string> synonyms, DateTime dateFounded)
+        public DateTime DateFounded { get; set; }
+        public string CEO { get; set; }
+        public int EmployeeCount { get; set; }
+        public Company(string name, List<string> synonyms, DateTime dateFounded, string ceo, int employeeCount)
         {
             this.Name = name;
             this.Synonyms = synonyms;
             this.DateFounded = dateFounded;
+            this.CEO = ceo;
+            this.EmployeeCount = employeeCount;
         }
-
         public string ToJson()
         {
             return JsonConvert.SerializeObject(this);
         }
-
         public static Company FromJson(string json)
         {
             return JsonConvert.DeserializeObject<Company>(json);
         }
-        public int GetAge() // @
+        public int GetAge()
         {
             return DateTime.Now.Year - DateFounded.Year;
+        }
+        public bool IsLargeCompany()
+        {
+            return EmployeeCount > 1000;
         }
     }
 }
