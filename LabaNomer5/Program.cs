5558e342a890a47f59838c1cd49fd1af0a7d1032
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-04-15 23:55:18 +0300

    Все працює, в рапорті покахує кількість згадокк майкрософт, ПЕРЕМОГА!!

--- LabaNomer5/Program.cs
+++ LabaNomer5/Program.cs
@@ -4,15 +4,28 @@
     {
         static void Main(string[] args)
         {
+            string baseDirectory = @"C:\Users\egorm\source\repos\LabaNomer5";
+            string newDirectory = Path.Combine(baseDirectory, "derictory");
+            string reportDirectory = Path.Combine(newDirectory, "report");
+
+            Directory.CreateDirectory(newDirectory);
+            Directory.CreateDirectory(reportDirectory);
+
+            string textFilePath = Path.Combine(newDirectory, "text.txt");
+
+            if (!File.Exists(textFilePath))
+            {
+                throw new FileNotFoundException($"Файл {textFilePath} не знайдено.");
+            }
             Company company = new Company("Microsoft", new List<string> { "MSFT", "Майкрософт" }, new DateTime(1975, 4, 4), "Satya Nadella", 163000);
 
             TextAnalyzer analyzer = new TextAnalyzer(company);
 
-            List<string> files = FileManager.GetFilesInDirectory(@"C:\Users\egorm\source\repos\LabaNomer5");
+            List<string> files = FileManager.GetFilesInDirectory(newDirectory);
 
             Dictionary<string, int> data = FileManager.AggregateData(files, analyzer);
 
-            ReportGenerator.GenerateReport(data, "report.json");
+            ReportGenerator.GenerateReport(data, Path.Combine(reportDirectory, "report.json"));
         }
     }
 }
