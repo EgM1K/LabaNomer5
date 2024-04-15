28fffd699ae2b399c0c0474d10995b56052c2269
EgM1k <23fmif.ye.mykhailenko@std.udu.edu.ua>
2024-04-15 22:57:56 +0300

    Всановив nuget package newtonesoft.json, та зробив клас компані( тільки базу(майже пустий))

--- LabaNomer5/Company.cs
+++ LabaNomer5/Company.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace LabaNomer5
 {
-    internal class Company
+    public class Company
     {
+        public string Name { get; set; }
+        public List<string> Synonyms { get; set; }
+
+        public Company(string name, List<string> synonyms)
+        {
+            this.Name = name;
+            this.Synonyms = synonyms;
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static Company FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<Company>(json);
+        }
     }
 }
